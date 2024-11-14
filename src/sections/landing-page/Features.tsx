dec5cc62065136ce9237e54e593cfdb59aba641a
Adesina <damiiyi@gmail.com>
2024-11-14 21:30:41 +0100

    updates

--- src/sections/landing-page/Features.tsx
+++ src/sections/landing-page/Features.tsx
@@ -1,61 +1,9 @@
 "use client";
 
 import Image from "next/image";
-import slackLogo from "@/assets/slack-logo.png";
-import dockerLogo from "@/assets/docker-logo.png";
-import figmaLogo from "@/assets/figma-logo.png";
-import githubLogo from "@/assets/github-logo.png";
-import vsCodeLogo from "@/assets/vs-code-logo.png";
-import notionLogo from "@/assets/notion-logo.png";
-import jiraLogo from "@/assets/jira-logo.png";
-import gcpLogo from "@/assets/gcp-logo.png";
-
 import ProductImage from "@/assets/product-image.png";
 
 import { ContainerScroll } from "@/components/ui/container-scroll-animation";
-
-export const logos = [
-  {
-    src: slackLogo,
-    alt: "slack logo",
-    rotate: 0,
-  },
-  {
-    src: dockerLogo,
-    alt: "docker logo",
-    rotate: 45,
-  },
-  {
-    src: figmaLogo,
-    alt: "figma logo",
-    rotate: 90,
-  },
-  {
-    src: githubLogo,
-    alt: "github logo",
-    rotate: 135,
-  },
-  {
-    src: vsCodeLogo,
-    alt: "vs code logo",
-    rotate: 180,
-  },
-  {
-    src: notionLogo,
-    alt: "notion logo",
-    rotate: 225,
-  },
-  {
-    src: jiraLogo,
-    alt: "jira logo",
-    rotate: 270,
-  },
-  {
-    src: gcpLogo,
-    alt: "gcp logo",
-    rotate: 315,
-  },
-];
 
 export const Features = () => {
   return (
